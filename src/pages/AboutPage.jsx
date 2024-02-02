4e4d17654e936eba07f978beb3835978ac4931f6
EdwinSch <edwinschutjes@gmail.com>
2024-02-02 18:38:17 +0100

    update about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,11 +1,42 @@
 import SubHeader from "../components/SubHeader/SubHeader";
 import Values from "../components/Value/Values";
+import InfoBullet from "../components/InfoBullet/InfoBullet";
+// Images
+import imgDigitalEra from "../assets/images/digital-era.jpg";
+import imgBetterLiving from "../assets/images/better-living.jpg";
+// Patterns
+import leftUpArrow from "../assets/patterns/left-upward-arrow.svg";
+import rightArrow from "../assets/patterns/right-arrow.svg";
 
 const AboutPage = () => {
   return (
     <>
       {/* Subheader */}
       <SubHeader text={"about"} />
+
+      {/* Info Bullets */}
+      <section>
+        {/* Bullet 1 */}
+        <InfoBullet
+          classlist={"info-bullet-container"}
+          image={imgDigitalEra}
+          arrowStyle={leftUpArrow}
+          title={"Mobility for the digital era"}
+          text={
+            "Getting around should be simple (and even fun!) for everyone. We embrace technology to provide low cost, smart access to scooters at your fingertips."
+          }
+        />
+        {/* Bullet 2 */}
+        <InfoBullet
+          classlist={"info-bullet-container inverse"}
+          image={imgBetterLiving}
+          arrowStyle={rightArrow}
+          title={"Better urban living"}
+          text={
+            "We’re helping connect cities and bring people closer together. Our scooters are also fully-electric and we offset the minimal carbon footprint for each ride."
+          }
+        />
+      </section>
 
       {/* Our values */}
       <Values title={"Our values"} />

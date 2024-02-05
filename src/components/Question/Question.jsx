9998049981008e683ba995635ec0a8e1c0de615b
EdwinSch <edwinschutjes@gmail.com>
2024-02-05 12:19:09 +0100

    toggle faq function

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa6";
 
 const Question = ({ id, question, answer }) => {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <article className="question-wrapper">
+    <article className="question-wrapper" onClick={() => setToggle(!toggle)}>
       <div className="question-header">
         <h4>{question}</h4>
-        <FaChevronDown className="faq-toggle-btn" />
+        {toggle ? (
+          <FaChevronUp className="faq-toggle-btn" />
+        ) : (
+          <FaChevronDown className="faq-toggle-btn" />
+        )}
       </div>
-      <p className="question-body">{answer}</p>
+
+      {toggle ? (
+        <p className="question-body">{answer}</p>
+      ) : (
+        <p className="question-body hide-answer">{answer}</p>
+      )}
     </article>
   );
 };
